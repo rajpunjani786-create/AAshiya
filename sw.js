eff2be06f591021770d61c62a382c8892c6e5d2e
rajpunjani786-create <rajpunjani786@gmail.com>
2026-06-10 11:10:39 +0530

    Add service worker with install and fetch event handlers

---
+++ sw.js
@@ -0,0 +1,8 @@
+```javascript
+self.addEventListener('install', (e) => {
+  console.log('Service Worker Installed');
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
